2207ffd7bde6405962618ad500ef8f03a2f83810
JunhaoQiu <56094690+qchiujunhao@users.noreply.github.com>
2024-07-16 17:04:26 -0400

    Adding actions for PR (#16)
    * updates for pr actions
    
    1. formart for planemo test and lint, and flask8 lint
    2. found and fixed bugs during planemo test
    3. added test-data files
    
    * Create .shed.yml
    
    * add details to .shed.yml
    
    * importing order
    
    * importing order
    
    * updated pr.yaml and import order
    
    1 for pr.yaml: commend checking file-size for now
    2. import order again

--- tools/dashboard.py
+++ tools/dashboard.py
@@ -1,9 +1,11 @@
-from typing import Any, Dict, Optional
 import logging
+from typing import Any, Dict, Optional
+
 from pycaret.utils.generic import get_label_encoder
 
 logging.basicConfig(level=logging.DEBUG)
 LOG = logging.getLogger(__name__)
+
 
 def generate_classifier_explainer_dashboard(
         exp,
@@ -11,13 +13,14 @@ def generate_classifier_explainer_dashboard(
         display_format: str = "dash",
         dashboard_kwargs: Optional[Dict[str, Any]] = None,
         run_kwargs: Optional[Dict[str, Any]] = None,
-        **kwargs,
-    ):
-        """
+        **kwargs,):
+
+    """
         This function is changed from pycaret.classification.oop.dashboard()
 
-        This function generates the interactive dashboard for a trained model. The
-        dashboard is implemented using ExplainerDashboard (explainerdashboard.readthedocs.io)
+        This function generates the interactive dashboard for a trained model.
+        The dashboard is implemented using
+        ExplainerDashboard (explainerdashboard.readthedocs.io)
 
 
         estimator: scikit-learn compatible object
@@ -25,13 +28,15 @@ def generate_classifier_explainer_dashboard(
 
 
         display_format: str, default = 'dash'
-            Render mode for the dashboard. The default is set to ``dash`` which will
+            Render mode for the dashboard. The default is set to ``dash``
+            which will
             render a dashboard in browser. There are four possible options:
 
             - 'dash' - displays the dashboard in browser
             - 'inline' - displays the dashboard in the jupyter notebook cell.
             - 'jupyterlab' - displays the dashboard in jupyterlab pane.
-            - 'external' - displays the dashboard in a separate tab. (use in Colab)
+            - 'external' - displays the dashboard in a separate tab.
+                (use in Colab)
 
 
         dashboard_kwargs: dict, default = {} (empty dict)
@@ -39,44 +44,48 @@ def generate_classifier_explainer_dashboard(
 
 
         run_kwargs: dict, default = {} (empty dict)
-            Dictionary of arguments passed to the ``run`` method of ``ExplainerDashboard``.
+            Dictionary of arguments passed to the ``run``
+            method of ``ExplainerDashboard``.
 
 
         **kwargs:
-            Additional keyword arguments to pass to the ``ClassifierExplainer`` or
-            ``RegressionExplainer`` class.
+            Additional keyword arguments to pass to the ``ClassifierExplainer``
+            or ``RegressionExplainer`` class.
 
 
         Returns:
             ExplainerDashboard
-        """
+    """
 
-        dashboard_kwargs = dashboard_kwargs or {}
-        run_kwargs = run_kwargs or {}
+    dashboard_kwargs = dashboard_kwargs or {}
+    run_kwargs = run_kwargs or {}
 
-        from explainerdashboard import ClassifierExplainer, ExplainerDashboard
+    from explainerdashboard import ClassifierExplainer, ExplainerDashboard
 
-        le = get_label_encoder(exp.pipeline)
-        if le:
-            labels_ = list(le.classes_)
-        else:
-            labels_ = None
+    le = get_label_encoder(exp.pipeline)
+    if le:
+        labels_ = list(le.classes_)
+    else:
+        labels_ = None
 
-        # Replaceing chars which dash doesnt accept for column name `.` , `{`, `}`
-        
-        X_test_df = exp.X_test_transformed.copy()
-        LOG.info(X_test_df)
-        X_test_df.columns = [
-            col.replace(".", "__").replace("{", "__").replace("}", "__")
-            for col in X_test_df.columns
-        ]
-        
-        explainer = ClassifierExplainer(
-            estimator, X_test_df, exp.y_test_transformed, labels=labels_, **kwargs
-        )
-        return ExplainerDashboard(
-            explainer, mode=display_format, contributions=False, whatif=False, **dashboard_kwargs
-        ) 
+    # Replaceing chars which dash doesnt accept for column name `.` , `{`, `}`
+
+    X_test_df = exp.X_test_transformed.copy()
+    LOG.info(X_test_df)
+    X_test_df.columns = [
+        col.replace(".", "__").replace("{", "__").replace("}", "__")
+        for col in X_test_df.columns
+    ]
+
+    explainer = ClassifierExplainer(
+        estimator, X_test_df, exp.y_test_transformed, labels=labels_, **kwargs
+    )
+    return ExplainerDashboard(
+        explainer, mode=display_format,
+        contributions=False, whatif=False,
+        **dashboard_kwargs
+    )
+
 
 def generate_regression_explainer_dashboard(
         exp,
@@ -84,13 +93,14 @@ def generate_regression_explainer_dashboard(
         display_format: str = "dash",
         dashboard_kwargs: Optional[Dict[str, Any]] = None,
         run_kwargs: Optional[Dict[str, Any]] = None,
-        **kwargs,
-    ):
-        """
-        This function is changed from pycaret.regression.oop.dashboard()
+        **kwargs,):
 
-        This function generates the interactive dashboard for a trained model. The
-        dashboard is implemented using ExplainerDashboard (explainerdashboard.readthedocs.io)
+    """
+    This function is changed from pycaret.regression.oop.dashboard()
+
+        This function generates the interactive dashboard for a trained model.
+        The dashboard is implemented using ExplainerDashboard
+        (explainerdashboard.readthedocs.io)
 
 
         estimator: scikit-learn compatible object
@@ -98,13 +108,15 @@ def generate_regression_explainer_dashboard(
 
 
         display_format: str, default = 'dash'
-            Render mode for the dashboard. The default is set to ``dash`` which will
+            Render mode for the dashboard. The default is set to ``dash``
+            which will
             render a dashboard in browser. There are four possible options:
 
             - 'dash' - displays the dashboard in browser
             - 'inline' - displays the dashboard in the jupyter notebook cell.
             - 'jupyterlab' - displays the dashboard in jupyterlab pane.
-            - 'external' - displays the dashboard in a separate tab. (use in Colab)
+            - 'external' - displays the dashboard in a separate tab.
+            (use in Colab)
 
 
         dashboard_kwargs: dict, default = {} (empty dict)
@@ -112,32 +124,36 @@ def generate_regression_explainer_dashboard(
 
 
         run_kwargs: dict, default = {} (empty dict)
-            Dictionary of arguments passed to the ``run`` method of ``ExplainerDashboard``.
+            Dictionary of arguments passed to the ``run`` method
+            of ``ExplainerDashboard``.
 
 
         **kwargs:
-            Additional keyword arguments to pass to the ``ClassifierExplainer`` or
+            Additional keyword arguments to pass to the
+            ``ClassifierExplainer`` or
             ``RegressionExplainer`` class.
 
 
         Returns:
             ExplainerDashboard
-        """
+    """
 
-        dashboard_kwargs = dashboard_kwargs or {}
-        run_kwargs = run_kwargs or {}
+    dashboard_kwargs = dashboard_kwargs or {}
+    run_kwargs = run_kwargs or {}
 
-        from explainerdashboard import ExplainerDashboard, RegressionExplainer
+    from explainerdashboard import ExplainerDashboard, RegressionExplainer
 
-        # Replaceing chars which dash doesnt accept for column name `.` , `{`, `}`
-        X_test_df = exp.X_test_transformed.copy()
-        X_test_df.columns = [
-            col.replace(".", "__").replace("{", "__").replace("}", "__")
-            for col in X_test_df.columns
-        ]
-        explainer = RegressionExplainer(
-            estimator, X_test_df, exp.y_test_transformed, **kwargs
-        )
-        return ExplainerDashboard(
-            explainer, mode=display_format, contributions=False, whatif=False, shap_interaction=False, decision_trees=False,  **dashboard_kwargs
-        )
+    # Replaceing chars which dash doesnt accept for column name `.` , `{`, `}`
+    X_test_df = exp.X_test_transformed.copy()
+    X_test_df.columns = [
+        col.replace(".", "__").replace("{", "__").replace("}", "__")
+        for col in X_test_df.columns
+    ]
+    explainer = RegressionExplainer(
+        estimator, X_test_df, exp.y_test_transformed, **kwargs
+    )
+    return ExplainerDashboard(
+        explainer, mode=display_format, contributions=False,
+        whatif=False, shap_interaction=False, decision_trees=False,
+        **dashboard_kwargs
+    )
--- tools/pycaret_regression.py
+++ tools/pycaret_regression.py
@@ -1,23 +1,35 @@
-from base_model_trainer import BaseModelTrainer
-from pycaret.regression import RegressionExperiment
-from dashboard import generate_regression_explainer_dashboard
 import logging
+
+from base_model_trainer import BaseModelTrainer
+
+from dashboard import generate_regression_explainer_dashboard
+
+from pycaret.regression import RegressionExperiment
 
 LOG = logging.getLogger(__name__)
 
+
 class RegressionModelTrainer(BaseModelTrainer):
-    def __init__(self, input_file, target_col, output_dir):
-        super().__init__(input_file, target_col, output_dir)
+    def __init__(self, input_file, target_col, output_dir,  **kwargs):
+        super().__init__(input_file, target_col, output_dir,  **kwargs)
         self.exp = RegressionExperiment()
-    
+
     def save_dashboard(self):
         LOG.info("Saving explainer dashboard")
-        dashboard = generate_regression_explainer_dashboard(self.exp, self.best_model)
+        dashboard = generate_regression_explainer_dashboard(self.exp,
+                                                            self.best_model)
         dashboard.save_html("dashboard.html")
 
     def generate_plots(self):
         LOG.info("Generating and saving plots")
-        plots = ['residuals', 'error', 'cooks', 'learning', 'vc', 'manifold', 'rfe', 'feature', 'feature_all']
+        plots = ['residuals', 'error', 'cooks',
+                 'learning', 'vc', 'manifold',
+                 'rfe', 'feature', 'feature_all']
         for plot_name in plots:
-            plot_path = self.exp.plot_model(self.best_model, plot=plot_name, save=True)
-            self.plots[plot_name] = plot_path
+            try:
+                plot_path = self.exp.plot_model(self.best_model,
+                                                plot=plot_name, save=True)
+                self.plots[plot_name] = plot_path
+            except Exception as e:
+                LOG.error(f"Error generating plot {plot_name}: {e}")
+                continue
